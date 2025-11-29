f3466ffc850b00c06e2a02d7a65497670bf363cd
jakepbleeden <jpbleeden@gmail.com>
2025-11-28 21:38:17 -0600

    Removed comment

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function requestPassword() {
     const pwd = prompt("Enter password to access protected content:");
 
-    // Temporary behavior — will be replaced with hashing + AES later
     if (pwd !== null) {
         console.log("Password entered:", pwd);
-        window.location.href = "protected.html";   // placeholder page
+        window.location.href = "protected.html";
     }
 }
